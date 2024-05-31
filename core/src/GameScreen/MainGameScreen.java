0ea46255ae1611f655755609e2adc141c6f4b6bf
ThisGabu <168417052+ThisGabu@users.noreply.github.com>
2024-05-31 16:39:03 +0700

    perapian dan pembubuatan class MainMenuScreen

---
+++ core/src/GameScreen/MainGameScreen.java
@@ -0,0 +1,7 @@
+package GameScreen;
+
+public class MainGameScreen {
+    public static final int widthScreen=1280;
+    public static final int heightScreen=720;
+
+}
---
+++ core/src/GameScreen/MainMenuScreen.java
@@ -0,0 +1,44 @@
+package GameScreen;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MainMenuScreen extends ApplicationAdapter{
+    public static final float width = MainGameScreen.widthScreen;
+    public static final float height = MainGameScreen.heightScreen;
+    public static final float xBackground = 0;
+    public static final float yBackgorund = 0;
+
+
+
+    Texture backgorund;
+    SpriteBatch batch;
+
+
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        backgorund = new Texture("background.png");
+    }
+
+    @Override
+    public void render () {
+
+        batch.begin();
+        batch.draw(backgorund, xBackground, yBackgorund, width, height);
+        batch.end();
+
+    }
+
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        backgorund.dispose();
+    }
+}
+
