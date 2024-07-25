48a0ffc5b3edc3295311dbc8adc47f0107ea774f
LoeisDavid <86642990+LoeisDavid@users.noreply.github.com>
2024-07-25 16:56:01 +0700

    menambahkan class intructionScreen

---
+++ core/src/GameScreen/IntructionScreen.java
@@ -0,0 +1,48 @@
+package GameScreen;
+
+import Entity.Player.ControllerPlayer;
+import com.badlogic.gdx.Screen;
+
+public class IntructionScreen implements Screen {
+
+    ControllerPlayer controllerPlayer;
+
+    IntructionScreen(){
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
