5fd55b740e2f29feae23d90b8565e6e95077a64d
LoeisDavid <86642990+LoeisDavid@users.noreply.github.com>
2024-07-27 19:49:36 +0700

    menyelesaikan insructionScreen menmabahkan hover pada tombol dalam InstructionScreen

--- core/src/GameScreen/InstructionScreen.java
+++ core/src/GameScreen/InstructionScreen.java
@@ -37,60 +37,70 @@ class InstructionScreen implements Screen {
     private final float yCenter= yInstruction+heightInstruction/2-heightCenter/3;
 
     Texture sButton;
+    Texture sHover;
     private final float widthS= 54*2;
     private final float heightS= 52*2;
     private final float xS= (xInstruction+widthInstruction)/4-widthS;
     private final float yS= (yInstruction+heightInstruction)/4;
 
     Texture aButton;
+    Texture aHover;
     private final float widthA= widthS;
     private final float heightA= heightS;
     private final float xA= xS-widthA-1;
     private final float yA= yS;
 
     Texture wButton;
+    Texture wHover;
     private final float widthW= widthA;
     private final float heightW= heightA;
     private final float xW= xS;
     private final float yW= yS+heightW+1;
 
     Texture dButton;
+    Texture dHover;
     private final float widthD= widthA;
     private final float heightD= heightA;
     private final float xD= xS+widthA+1;
     private final float yD= yS;
 
     Texture spaceButton;
+    Texture spaceHover;
     private final float widthSpace= 202*2;
     private final float heightSpace= 52*2;
     private final float xSpace= xD+widthS+1;
     private final float ySpace= yS;
 
     Texture downButton;
+    Texture downHover;
     private final float widthDown= 54*2;
     private final float heightDown= 52*2;
     private final float xDown= (xInstruction+widthInstruction)-(xS-xInstruction)-widthDown;
     private final float yDown= yS;
 
     Texture upButton;
+    Texture upHover;
     private final float widthUp= widthA;
     private final float heightUp= heightA;
     private final float xUp= xDown;
     private final float yUp= yDown+heightDown+1;
 
     Texture rightButton;
+    Texture rightHover;
     private final float widthRight= widthA;
     private final float heightRight= heightA;
     private final float xRight= xDown+widthRight+1;
     private final float yRight= yDown;
 
     Texture leftButton;
+    Texture leftHover;
     private final float widthLeft= widthA;
     private final float heightLeft= heightA;
     private final float xLeft= xDown-widthDown-1;
     private final float yLeft= yDown;
 
     Texture ctrlButton;
+    Texture ctrlHover;
     private final float widthCTRL= 135*2;
     private final float heightCTRL= 52*2;
     private final float xCTRL= xLeft-widthCTRL-1;
@@ -110,11 +120,23 @@ class InstructionScreen implements Screen {
         dButton= new Texture("..\\bomberMan\\assets\\IntructionScreen\\KeyboardD.png");
         spaceButton= new Texture("..\\bomberMan\\assets\\IntructionScreen\\KeyboardSpace.png");
 
+        aHover= new Texture("..\\bomberMan\\assets\\IntructionScreen\\KeyboardAHover.png");
+        wHover= new Texture("..\\bomberMan\\assets\\IntructionScreen\\KeyboardWHover.png");
+        sHover= new Texture("..\\bomberMan\\assets\\IntructionScreen\\KeyboardSHover.png");
+        dHover= new Texture("..\\bomberMan\\assets\\IntructionScreen\\KeyboardDHover.png");
+        spaceHover= new Texture("..\\bomberMan\\assets\\IntructionScreen\\SpaceHover.png");
+
         upButton= new Texture("..\\bomberMan\\assets\\IntructionScreen\\arrowUp.png");
         downButton= new Texture("..\\bomberMan\\assets\\IntructionScreen\\arrowDown.png");
         rightButton= new Texture("..\\bomberMan\\assets\\IntructionScreen\\arrowRight.png");
         leftButton= new Texture("..\\bomberMan\\assets\\IntructionScreen\\arrowLeft.png");
         ctrlButton= new Texture("..\\bomberMan\\assets\\IntructionScreen\\keyboardCTRL.png");
+
+        upHover= new Texture("..\\bomberMan\\assets\\IntructionScreen\\ArrowUpHover.png");
+        downHover= new Texture("..\\bomberMan\\assets\\IntructionScreen\\ArrowDownHover.png");
+        rightHover= new Texture("..\\bomberMan\\assets\\IntructionScreen\\ArrowRightHover.png");
+        leftHover= new Texture("..\\bomberMan\\assets\\IntructionScreen\\ArrowLeftHover.png");
+        ctrlHover= new Texture("..\\bomberMan\\assets\\IntructionScreen\\CTRLHover.png");
 
         spawnTile= new SpawnTile[PlayScreen.jumlahPlayer];
         batch= new SpriteBatch();
@@ -152,16 +174,66 @@ class InstructionScreen implements Screen {
         batch.draw(backgorund,xBackground,yBackground,widthBackground,heightBackground);
         batch.draw(instructionBackground,xInstruction,yInstruction,widthInstruction,heightInstruction);
         batch.draw(center, xCenter, yCenter, widthCenter, heightCenter);
-        batch.draw(aButton,xA,yA,widthA,heightA);
-        batch.draw(wButton,xW,yW,widthW,heightW);
-        batch.draw(sButton, xS, yS, widthS, heightS);
-        batch.draw(dButton, xD, yD, widthD, heightD);
-        batch.draw(spaceButton, xSpace, ySpace, widthSpace, heightSpace);
-        batch.draw(upButton, xUp, yUp, widthUp, heightUp);
-        batch.draw(downButton, xDown, yDown, widthDown, heightDown);
-        batch.draw(rightButton, xRight, yRight, widthRight, heightRight);
-        batch.draw(leftButton, xLeft, yLeft, widthLeft, heightLeft);
-        batch.draw(ctrlButton, xCTRL, yCTRL, widthCTRL, heightCTRL);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A)){
+            batch.draw(aHover,xA,yA,widthA,heightA);
+        } else {
+            batch.draw(aButton,xA,yA,widthA,heightA);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.W)){
+            batch.draw(wHover,xW,yW,widthW,heightW);
+        } else {
+            batch.draw(wButton,xW,yW,widthW,heightW);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.S)){
+            batch.draw(sHover, xS, yS, widthS, heightS);
+        } else {
+            batch.draw(sButton, xS, yS, widthS, heightS);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.D)){
+            batch.draw(dHover, xD, yD, widthD, heightD);
+        } else {
+            batch.draw(dButton, xD, yD, widthD, heightD);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+            batch.draw(spaceHover, xSpace, ySpace, widthSpace, heightSpace);
+        } else {
+            batch.draw(spaceButton, xSpace, ySpace, widthSpace, heightSpace);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)){
+            batch.draw(upHover, xUp, yUp, widthUp, heightUp);
+        } else {
+            batch.draw(upButton, xUp, yUp, widthUp, heightUp);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            batch.draw(downHover, xDown, yDown, widthDown, heightDown);
+        } else {
+            batch.draw(downButton, xDown, yDown, widthDown, heightDown);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            batch.draw(rightHover, xRight, yRight, widthRight, heightRight);
+        } else {
+            batch.draw(rightButton, xRight, yRight, widthRight, heightRight);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            batch.draw(leftHover, xLeft, yLeft, widthLeft, heightLeft);
+        } else {
+            batch.draw(leftButton, xLeft, yLeft, widthLeft, heightLeft);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT)){
+            batch.draw(ctrlHover, xCTRL, yCTRL, widthCTRL, heightCTRL);
+        } else {
+            batch.draw(ctrlButton, xCTRL, yCTRL, widthCTRL, heightCTRL);
+        }
 
         for (int i= 0; i<PlayScreen.jumlahPlayer; i++){
             player.update(i,map,delta);
@@ -180,6 +252,7 @@ class InstructionScreen implements Screen {
                 }
             }
         }
+
 
         for (int i=0; i<PlayScreen.jumlahPlayer; i++){
             if (player.getYPositionPlayer(i)+player.getHeightPlayer(i)<yCenter+heightCenter){
